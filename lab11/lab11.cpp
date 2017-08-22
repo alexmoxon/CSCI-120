149e028ef600f7f08351f15683f6dc356518b176
Alex Moxon <amoxon001@student.butte.edu>
2017-08-22 22:13:42 +0000

    lab1.1 submission

---
+++ lab11/lab11.cpp
@@ -0,0 +1,15 @@
+step 1: program tells the user how many points he currently has.
+step 2: program randomly selects number between 1 & 10.
+step 3: program asks the user to select a number between 1 & 10.
+step 4: user inputs his number.
+step 5: program checks if number is within set range.
+step 6: if not, asks again for another number within range.
+step 7: if number is within range, program checks to see if it's correct.
+step 8: if user guesses correct number, program gives him congratulations message 
+& program starts back over from the begginning without loss of a point.
+step 9: if guessed number isn't correct, program removes one point from score & 
+gives user feedback on wether its higher or lower than its randomly selected number.
+step 10: program asks user to guess another number.
+step 11: if correct program jumps back to the top with an updated points total.
+step 11: if incorrect program removes another point from total and the process 
+is repeated until user is either out of points our guesses the correct number.
